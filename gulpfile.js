962d6724b03412ec385d647dd0d27e3136e3b3c4
girgir <wjdrlf5986@naver.com>
2025-03-28 14:28:32 +0900

    모던 Sass 모듈 시스템으로 변경 및 gulp-sass 버전 업데이트

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,43 @@
 // ----- Imports and variables ------
-const { src, dest, watch, series, parallel, lastRun } = require('gulp');
-const gulpLoadPlugins = require('gulp-load-plugins');
+const { src, dest, watch, series, parallel, lastRun } = require("gulp");
+const gulpLoadPlugins = require("gulp-load-plugins");
 const $ = gulpLoadPlugins();
-const browserSync = require('browser-sync');
+const browserSync = require("browser-sync");
 const server = browserSync.create();
-const del = require('del');
-const autoprefixer = require('autoprefixer');
+const del = require("del");
+const autoprefixer = require("autoprefixer");
+const gulpSass = require("gulp-sass");
+const dartSass = require("sass");
+const sass = gulpSass(dartSass);
 
 const paths = {
-  src: 'src',
-  dest: 'docs',
-  tmp: '.tmp'
+  src: "src",
+  dest: "docs",
+  tmp: ".tmp",
 };
-
 
 // ----- Tasks ------
 function styles() {
   return src(`${paths.src}/styles/*.scss`)
     .pipe($.plumber())
     .pipe($.sourcemaps.init())
-    .pipe($.sass.sync({
-      outputStyle: 'expanded',
-      precision: 10,
-      includePaths: ['.']
-    })
-    .on('error', $.sass.logError))
-    .pipe($.postcss([
-      autoprefixer()
-    ]))
+    .pipe(
+      sass
+        .sync({
+          outputStyle: "expanded",
+          precision: 10,
+          includePaths: ["."],
+          implementation: dartSass,
+        })
+        .on("error", sass.logError)
+    )
+    .pipe($.postcss([autoprefixer()]))
     .pipe($.sourcemaps.write())
     .pipe(dest(`${paths.tmp}/styles`))
-    .pipe(server.reload({stream: true}));
-};
+    .pipe(server.reload({ stream: true }));
+}
 
 exports.styles = styles;
-
 
 // ----- Serve tasks ------
 function startAppServer() {
@@ -44,34 +47,32 @@ function startAppServer() {
     server: {
       baseDir: [`${paths.tmp}`, `${paths.src}`],
       routes: {
-        '/node_modules': 'node_modules'
+        "/node_modules": "node_modules",
       },
       serveStaticOptions: {
-        extensions: ['html']
-      }
-    }
+        extensions: ["html"],
+      },
+    },
   });
 
-  watch([
-    `${paths.src}/*.html`
-  ]).on('change', server.reload);
+  watch([`${paths.src}/*.html`]).on("change", server.reload);
 
   watch(`${paths.src}/**/*.scss`, styles);
 }
 
-
 let serve = series(clean, styles, startAppServer);
 exports.serve = serve;
 
-
 // ----- Build tasks ------
 function compress() {
-  return src([`${paths.tmp}/*/**/*.{html,css,js}`, `${paths.src}/**/*.{html,js,jpg,gif,png}`])
-    .pipe(dest(`${paths.dest}`));
+  return src([
+    `${paths.tmp}/*/**/*.{html,css,js}`,
+    `${paths.src}/**/*.{html,js,jpg,gif,png}`,
+  ]).pipe(dest(`${paths.dest}`));
 }
 
 function clean() {
-  return del([`${paths.tmp}`, `${paths.dest}`])
+  return del([`${paths.tmp}`, `${paths.dest}`]);
 }
 
 exports.clean = clean;
